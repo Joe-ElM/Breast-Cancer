c460280b1f2feb18ffb174af432dbe366572c2c5
joe el <asome.joe123@gmail.com>
2024-01-19 11:14:42 +0100

    First commit

---
+++ Notebooks/Modules.py
@@ -0,0 +1,31 @@
+import pandas                  as     pd
+import numpy                   as     np
+import seaborn                 as     sns
+import matplotlib.pyplot       as     plt
+import matplotlib.ticker       as     ticker
+import math
+import                                pickle
+import                                warnings
+
+from   matplotlib.lines        import Line2D
+from   sklearn.preprocessing   import StandardScaler, OneHotEncoder, PolynomialFeatures, RobustScaler, Normalizer
+from   sklearn.svm             import SVC
+from   sklearn.impute          import SimpleImputer
+from   sklearn.model_selection import train_test_split, GridSearchCV, cross_validate                         
+from   sklearn.pipeline        import Pipeline
+from   sklearn.metrics         import precision_score, recall_score, accuracy_score, f1_score, make_scorer
+from   sklearn.metrics         import confusion_matrix, euclidean_distances, precision_recall_curve, roc_curve, auc
+from   sklearn.ensemble        import RandomForestClassifier, VotingClassifier
+from   sklearn.compose         import ColumnTransformer
+from   sklearn.tree            import DecisionTreeClassifier, plot_tree
+from   sklearn.neighbors       import KNeighborsClassifier
+from   sklearn.linear_model    import LogisticRegression
+from   sklearn.cluster         import DBSCAN, KMeans
+from   sklearn.decomposition   import PCA
+from   sklearn.exceptions      import ConvergenceWarning
+
+
+
+from   imblearn.over_sampling  import SMOTE, RandomOverSampler
+from   imblearn.under_sampling import RandomUnderSampler
+from   imblearn.pipeline       import Pipeline as imbPipeline  
